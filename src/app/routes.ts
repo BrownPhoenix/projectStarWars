d374cf5084ca6c738d63903eb0add46f528f7323
BrownPhoenix <lotterannika@gmail.com>
2019-10-01 10:42:06 +0200

    Auto-generated commit

--- src/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { SwapiComponent } from './swapi/swapi.component';
-
-export const allAppRoutes: Routes =[
-    {path: '', component: SwapiComponent}
-];
